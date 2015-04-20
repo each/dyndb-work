30435e6b2345dfadb680da6a1c4c7df1a8318b89
Petr Spacek <pspacek@redhat.com>
2015-04-20 15:37:13 +0200

    [rt35271a] test two dyndb instances inside one named process

--- bin/tests/system/dyndb/tests.sh
+++ bin/tests/system/dyndb/tests.sh
@@ -115,6 +115,9 @@ status=`expr $status + $ret`
 test_add test3.ipv4.example.nil. A "10.53.0.12" || ret=1
 status=`expr $status + $ret`
 
+test_add test4.ipv6.example.nil. AAAA "2001:db8::1" || ret=1
+status=`expr $status + $ret`
+
 test_del test3.ipv4.example.nil. A || ret=1
 status=`expr $status + $ret`
 
@@ -122,6 +125,9 @@ test_del test2.ipv4.example.nil. A || ret=1
 status=`expr $status + $ret`
 
 test_del test1.ipv4.example.nil. A || ret=1
+status=`expr $status + $ret`
+
+test_del test4.ipv6.example.nil. AAAA || ret=1
 status=`expr $status + $ret`
 
 exit $status
