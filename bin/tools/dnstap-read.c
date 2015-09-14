254fb51ef00f9b1392b9777ca3cddec3a4aa3b59
Evan Hunt <each@isc.org>
2015-09-13 22:34:05 -0700

    [rt40211] release note and licenses

--- bin/tools/dnstap-read.c
+++ bin/tools/dnstap-read.c
@@ -13,6 +13,23 @@
  * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  */
+/*
+ * Portions of this code were adapted from dnstap-ldns:
+ *
+ * Copyright (c) 2014 by Farsight Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <config.h>
 
@@ -99,7 +116,6 @@ dnstap_file(struct fstrm_reader *r) {
 
 	return (ISC_FALSE);
 }
-
 
 static void
 print_yaml(dns_dtdata_t *d) {
